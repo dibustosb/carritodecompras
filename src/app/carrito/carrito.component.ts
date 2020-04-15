25ebf18d4ac5f7f83801973ff7f7bb86f874af25
Diego Bustos <diego.bustos@kibernum.com>
2020-04-15 19:05:11 -0400

    se comenta el código y se agrega contador de prodcutos

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -8,15 +8,22 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 export class CarritoComponent implements OnInit {
 	@ViewChild('correo', {static: false}) correoElement: ElementRef;
+
+	//determina si el usuario está logueado o no
 	usuarioLogueado:boolean;
-	correo_valido:string = 'diego.bustos@kibernum.com';
+
+	//correos válidos que permite el ingreso
 	correos_validos = [
 		'diego.bustos@kibernum.com',
 		'diego.bustos@gmail.com',
 		'diego@email.com',
 		'sergio.sepulveda@dev-solutions.cl'
 	];
+
+	//aquí se guadará el correo una vez que ingrese
 	correo:string;
+
+	//se define lista de productos disponibles para comprar
 	productos = [
 		{id:1, nombre: 'Tablet Guahuei', valor: 35000, disponible: true, img: 'assets/productos/tablet.jpg'},
 		{id:2, nombre: 'Smartphone Chiaomi', valor: 75000, disponible: true, img: 'assets/productos/smartphone.jpg'},
@@ -24,7 +31,10 @@ export class CarritoComponent implements OnInit {
 		{id:4, nombre: 'Audifonos Zony', valor: 12500, disponible: true, img: 'assets/productos/audifonos.jpg'},
 		{id:5, nombre: 'Notebook I-mak Pear', valor: 450000, disponible: true, img: 'assets/productos/notebook.jpg'}
 	];
+
+	//arreglo donde irán las compras
 	compras = [];
+
 	producto = [];
 	indice:number;
 	mensaje_error:string;
@@ -46,10 +56,12 @@ export class CarritoComponent implements OnInit {
 		}
 	}
 
+	//obtiene el correo del usuario que está logueado
 	getCorreoLogueado(){
 		return this.correo;
 	}
 
+	//método para agregar los productos
 	agregarProducto(id:number){
 		this.indice = id-1;
 		this.compras.push({
@@ -57,7 +69,8 @@ export class CarritoComponent implements OnInit {
 			nombre: this.productos[this.indice].nombre,
 			valor: this.productos[this.indice].valor
 		});
-		this.total = this.total + this.productos[this.indice].valor;
+		this.total += this.productos[this.indice].valor;
+		console.log(this.total);
 		this.productos[this.indice].disponible = false;
 	}
 
@@ -80,16 +93,20 @@ export class CarritoComponent implements OnInit {
 		this.productos[indiceProductos].disponible = true;
 	}
 
+	//método para salir del carrito de compras
 	logout(){
 		this.usuarioLogueado = false;
 		this.correoElement.nativeElement.value = "";
 		this.compras = [];
+
+		//recorre los productos y los deja todos disponibles
 		for( var p in this.productos){
 			this.productos[p]['disponible'] = true;
 		}
 		this.mensaje_error = "";
 	}
 
+	//método para mostrar un mensaje de error en el login
 	getMensajeError(){
 		return this.mensaje_error;
 	}
